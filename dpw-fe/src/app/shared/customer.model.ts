ca8fd74d36385cd40550abaae49e35af3697726c
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-06-05 12:12:46 -0500

    reset

--- dpw-fe/src/app/shared/customer.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Customer {
-  id: number;
-  email: string;
-  name?: string;
-  description?: string;
-  stripeCustomerId?: string;
-  defaultPaymentMethodId?: string;
-  metadata?: { [key: string]: string };
-}
